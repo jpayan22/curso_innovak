766513d91eea4ef517c248a63cfde6e1f4771107
Jorge Daniel Payan Almanza <jpayan@innovak.int>
2023-10-03 16:03:38 -0600

    Mi primer script

---
+++ Scripts/01_Buenas_practicas.R
@@ -0,0 +1,12 @@
+####buenas prácticas de programación ####
+
+### Paquetes o Librerias ###
+
+## Paquetes que necesitan ser instalados 
+
+### datos ###
+
+library(dplyr) # paquete diferencias significativas
+# además me ayuda colocando flechas 
+
+## cuando me corran ## 
